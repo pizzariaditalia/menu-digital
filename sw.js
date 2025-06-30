63c3ab5697b0f9c89ff254c4a831d3bc805489c1
PizzariaDitalia <61949944+pizzariaditalia@users.noreply.github.com>
2025-06-30 17:51:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'ditalia-pizzaria-cache-v203'; // Mudei a versão mais uma vez
+const CACHE_NAME = 'ditalia-pizzaria-cache-v204'; // Mudei a versão mais uma vez
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
