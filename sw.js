db0d90e51c0e427fa63f19af5044987265d1b778
PizzariaDitalia <61949944+pizzariaditalia@users.noreply.github.com>
2025-07-02 00:54:30 -0300

    update sw.js

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMaD6Z3CDxdkyzQXHpV3b0QBWr--xQTso",
-    authDomain: "app-ditalia.firebaseapp.com",
-    projectId: "app-ditalia",
-    storageBucket: "app-ditalia.firebasestorage.app",
-    messagingSenderId: "122567535166",
-    appId: "1:122567535166:web:19de7b8925042027063f6f",
-    measurementId: "G-5QW3MVGYME"
+  apiKey: "AIzaSyDMaD6Z3CDxdkyzQXHpV3b0QBWr--xQTso",
+  authDomain: "app-ditalia.firebaseapp.com",
+  projectId: "app-ditalia",
+  storageBucket: "app-ditalia.firebasestorage.app",
+  messagingSenderId: "122567535166",
+  appId: "1:122567535166:web:19de7b8925042027063f6f",
+  measurementId: "G-5QW3MVGYME"
 };
 
 console.log("[sw.js] Service Worker está sendo inicializado com a seguinte configuração:", firebaseConfig);
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'ditalia-pizzaria-cache-v251'; // Mudei a versão mais uma vez
+const CACHE_NAME = 'ditalia-pizzaria-cache-v252'; // Mudei a versão mais uma vez
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
@@ -49,25 +49,25 @@ self.addEventListener('install', (event) => {
   console.log('[sw.js] Evento de instalação disparado (v73).');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[sw.js] Cache aberto, adicionando URLs ao cache.');
-        return cache.addAll(URLS_TO_CACHE);
-      })
-      .catch((error) => {
-        console.error('[sw.js] Falha ao adicionar arquivos ao cache durante a instalação:', error);
-      })
+    .then((cache) => {
+      console.log('[sw.js] Cache aberto, adicionando URLs ao cache.');
+      return cache.addAll(URLS_TO_CACHE);
+    })
+    .catch((error) => {
+      console.error('[sw.js] Falha ao adicionar arquivos ao cache durante a instalação:', error);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    .then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
 });
 
