30754dbd4b951b6b9bcbf1da57ff5ada67363b7a
PizzariaDitalia <61949944+pizzariaditalia@users.noreply.github.com>
2025-07-05 12:14:35 -0300

    update entregador/sw-entregador.js

--- entregador/sw-entregador.js
+++ entregador/sw-entregador.js
@@ -40,7 +40,7 @@ messaging.onBackgroundMessage(function(payload) {
 
 
 // 5. Lógica de Cache do PWA (Sua lógica original, com a versão do cache atualizada)
-const CACHE_NAME = 'ditalia-entregador-cache-v69'; // Versão incrementada
+const CACHE_NAME = 'ditalia-entregador-cache-v70'; // Versão incrementada
 const URLS_TO_CACHE = [
   './',
   './login.html',
