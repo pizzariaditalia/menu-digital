3f537a705cf8c890fb520ee14785d088995b5afb
PizzariaDitalia <61949944+pizzariaditalia@users.noreply.github.com>
2025-07-26 18:27:44 -0300

    update Admin/sw-admin.js

--- Admin/sw-admin.js
+++ Admin/sw-admin.js
@@ -31,7 +31,7 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'ditalia-admin-cache-v252';
+const CACHE_NAME = 'ditalia-admin-cache-v253';
 const URLS_TO_CACHE = [
   './login.html',
   './auth.js',
